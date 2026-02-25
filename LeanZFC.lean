16b73737cedc6682616d7d56638525162dfdf5f8
Lean 4 VS Code Extension <>
2026-02-25 17:18:13 +0530

    Initial commit

---
+++ LeanZFC.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `LeanZFC` library.
+-- Import modules here that should be built as part of the library.
+import LeanZFC.Basic
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import LeanZFC
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
