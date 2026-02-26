313700c9f4036643fa114c180788047c37fe4bee
Xavier X <amritmahendrajoseph@gmail.com>
2026-02-26 11:09:43 +0530

    some more theorems

--- LeanZFC/Main.lean
+++ LeanZFC/Main.lean
@@ -75,8 +75,42 @@ theorem or_elim {P Q R : Prop} (h1 : P ∨ Q) (h2 : P → R) (h3 : Q → R) : R 
 
 theorem and_intro {P Q : Prop} (h1 : P) (h2 : Q) : P ∧ Q := λ h3 => or_elim h3 (λ h4 => h4 h1) (λ h4 => h4 h2)
 
-theorem and_elim_left {P Q : Prop} (h : P ∧ Q) : P := sorry
+theorem and_elim_left {P Q : Prop} (h : P ∧ Q) : P := not_not_elim # λ h1 => h # or_intro_left h1
 
-theorem and_elim_right {P Q : Prop} (h : P ∧ Q) : Q := sorry
+theorem and_elim_right {P Q : Prop} (h : P ∧ Q) : Q := not_not_elim # λ h1 => h # or_intro_right h1
+
+theorem iff_intro {P Q : Prop} (h1 : P → Q) (h2 : Q → P) : P ↔ Q := and_intro h1 h2
+
+theorem iff_elim_left {P Q : Prop} (h : P ↔ Q) : P → Q := and_elim_left h
+
+theorem iff_elim_right {P Q : Prop} (h : P ↔ Q) : Q → P := and_elim_right h
+
+theorem true_ne_false : true ≠ false := λ h => h id trivial
+
+theorem not_not {P : Prop} : ¬¬P ↔ P := iff_intro not_not_elim not_not_intro
+
+theorem iff_rec (F : Prop → Prop) {P Q : Prop} (h₁ : P ↔ Q) (h₂ : F P) : F Q := sorry
+
+theorem eq_refl {α : Type} (x : α) : x = x := λ _ => id
+
+theorem eq_symm {α : Type} {x y : α} (h : x = y) : y = x := sorry
+
+theorem eq_iff {α : Type} {x y : α} : x = y ↔ (∀ P : α → Prop, P x ↔ P y) := sorry
+
+theorem eq_rec {α : Type} (P : α → Prop) {x y : α} (h1 : x = y) (h2 : P x) : P y := sorry
+
+theorem imp_refl {P : Prop} : P → P := id
+
+theorem iff_refl {P : Prop} : P ↔ P := iff_intro id id
+
+theorem iff_symm {P Q : Prop} (h : P ↔ Q) : Q ↔ P := iff_intro (iff_elim_right h) (iff_elim_left h)
+
+theorem or_self {P : Prop} : P ∨ P ↔ P := sorry
+
+theorem or_symm {P Q : Prop} (h : P ∨ Q) : Q ∨ P := sorry
+
+theorem and_self {P : Prop} : P ∧ P ↔ P := sorry
+
+theorem and_symm {P Q : Prop} (h : P ∧ Q) : Q ∧ P := sorry
 
 end ZFC
