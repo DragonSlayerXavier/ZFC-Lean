b27d607316aec453ca905d99c7871ea516b225df
Xavier X <amritmahendrajoseph@gmail.com>
2026-03-05 20:50:30 +0530

    Propositional Logic done

--- Propositional/Prop_Classical.lean
+++ Propositional/Prop_Classical.lean
@@ -14,39 +14,263 @@ axiom double_neg_elim (A : Formula) : is_provable (¬¬A → A)
 
 -- Theorems
 theorem proof_by_contradiction (A : Formula) : is_provable ((¬A → Formula.bot) → A) := by
-  sorry
+  apply deduction
+  intro h
+  have hnna: is_provable (¬¬A) := by
+    apply mp (neg_intro (¬A))
+    apply deduction
+    intro hna
+    exact mp h hna
+  exact mp (double_neg_elim A) hnna
 
 theorem ex_falso_classical (A : Formula) : is_provable (Formula.bot → A) := by
-  sorry
+  apply deduction
+  intro h
+  have hnna: is_provable (¬¬A) := by
+    apply mp (neg_intro (¬A))
+    apply deduction
+    intro hna
+    exact h
+  exact mp (double_neg_elim A) hnna
 
 theorem excluded_middle (A : Formula) : is_provable (A ∨ ¬A) := by
-  sorry
+  apply mp (proof_by_contradiction (A ∨ ¬A))
+  apply deduction
+  intro h
+  have hna: is_provable (¬A) := by
+    apply mp (neg_intro A)
+    apply deduction
+    intro ha
+    have h_or: is_provable (A ∨ ¬A) := by
+      exact mp (disj_intro_left A (¬A)) ha
+    exact mp (mp (neg_elim (A ∨ ¬A)) h) h_or
+  have h_or: is_provable (A ∨ ¬A) := by
+    exact mp (disj_intro_right A (¬A)) hna
+  exact mp (mp (neg_elim (A ∨ ¬A)) h) h_or
 
 theorem double_neg_equiv (A : Formula) : is_provable (A ↔ ¬¬A) := by
-  sorry
+  have hfwd : is_provable (A → ¬¬A) := by
+    apply deduction
+    intro ha
+    apply mp (neg_intro (¬A))
+    apply deduction
+    intro hna
+    exact mp (mp (neg_elim A) hna) ha
+  have hbwd : is_provable (¬¬A → A) := by
+    exact double_neg_elim A
+  exact mp (mp (iff_intro A (¬¬A)) hfwd) hbwd
 
 theorem disj_iff_not_conj_not (A B : Formula) : is_provable ((A ∨ B) ↔ ¬(¬A ∧ ¬B)) := by
-  sorry
+  have hfwd : is_provable ((A ∨ B) → ¬(¬A ∧ ¬B)) := by
+    apply deduction
+    intro hor
+    apply mp (neg_intro (¬A ∧ ¬B))
+    apply deduction
+    intro hnanb
+    have hna : is_provable (¬A) := mp (conj_elim_left (¬A) (¬B)) hnanb
+    have hnb : is_provable (¬B) := mp (conj_elim_right (¬A) (¬B)) hnanb
+    have ha_bot : is_provable (A → Formula.bot) := by
+      apply deduction
+      intro ha
+      exact mp (mp (neg_elim A) hna) ha
+    have hb_bot : is_provable (B → Formula.bot) := by
+      apply deduction
+      intro hb
+      exact mp (mp (neg_elim B) hnb) hb
+    exact mp (mp (mp (disj_elim A B Formula.bot) ha_bot) hb_bot) hor
+  have hbwd : is_provable (¬(¬A ∧ ¬B) → (A ∨ B)) := by
+    apply deduction
+    intro h
+    apply mp (proof_by_contradiction (A ∨ B))
+    apply deduction
+    intro n_aorb
+    have hna : is_provable (¬A) := by
+      apply mp (neg_intro A)
+      apply deduction
+      intro ha
+      exact mp (mp (neg_elim (A ∨ B)) n_aorb) (mp (disj_intro_left A B) ha)
+    have hnb : is_provable (¬B) := by
+      apply mp (neg_intro B)
+      apply deduction
+      intro hb
+      exact mp (mp (neg_elim (A ∨ B)) n_aorb) (mp (disj_intro_right A B) hb)
+    have hnanb : is_provable (¬A ∧ ¬B) := mp (mp (conj_intro (¬A) (¬B)) hna) hnb
+    exact mp (mp (neg_elim (¬A ∧ ¬B)) h) hnanb
+  exact mp (mp (iff_intro (A ∨ B) (¬(¬A ∧ ¬B))) hfwd) hbwd
 
 theorem conj_iff_not_disj_not (A B : Formula) : is_provable ((A ∧ B) ↔ ¬(¬A ∨ ¬B)) := by
-  sorry
+  have hfwd : is_provable ((A ∧ B) → ¬(¬A ∨ ¬B)) := by
+    apply deduction
+    intro hab
+    apply mp (neg_intro (¬A ∨ ¬B))
+    apply deduction
+    intro hnanb
+    have ha : is_provable A := mp (conj_elim_left A B) hab
+    have hb : is_provable B := mp (conj_elim_right A B) hab
+    have hna_bot : is_provable (¬A → Formula.bot) := by
+      apply deduction
+      intro hna
+      exact mp (mp (neg_elim A) hna) ha
+    have hnb_bot : is_provable (¬B → Formula.bot) := by
+      apply deduction
+      intro hnb
+      exact mp (mp (neg_elim B) hnb) hb
+    exact mp (mp (mp (disj_elim (¬A) (¬B) Formula.bot) hna_bot) hnb_bot) hnanb
+  have hbwd : is_provable (¬(¬A ∨ ¬B) → (A ∧ B)) := by
+    apply deduction
+    intro h
+    have ha : is_provable A := by
+      apply mp (proof_by_contradiction A)
+      apply deduction
+      intro hna
+      exact mp (mp (neg_elim (¬A ∨ ¬B)) h) (mp (disj_intro_left (¬A) (¬B)) hna)
+    have hb : is_provable B := by
+      apply mp (proof_by_contradiction B)
+      apply deduction
+      intro hnb
+      exact mp (mp (neg_elim (¬A ∨ ¬B)) h) (mp (disj_intro_right (¬A) (¬B)) hnb)
+    exact mp (mp (conj_intro A B) ha) hb
+  exact mp (mp (iff_intro (A ∧ B) (¬(¬A ∨ ¬B))) hfwd) hbwd
 
 theorem imp_iff_not_disj (A B : Formula) : is_provable ((A → B) ↔ (¬A ∨ B)) := by
-  sorry
+  have hfwd : is_provable ((A → B) → (¬A ∨ B)) := by
+    apply deduction
+    intro hab
+    apply mp (proof_by_contradiction (¬A ∨ B))
+    apply deduction
+    intro h_not_disj
+    have hna : is_provable (¬A) := by
+      apply mp (neg_intro A)
+      apply deduction
+      intro ha
+      have hb : is_provable B := mp hab ha
+      exact mp (mp (neg_elim (¬A ∨ B)) h_not_disj) (mp (disj_intro_right (¬A) B) hb)
+    exact mp (mp (neg_elim (¬A ∨ B)) h_not_disj) (mp (disj_intro_left (¬A) B) hna)
+  have hbwd : is_provable ((¬A ∨ B) → (A → B)) := by
+    apply deduction
+    intro hor
+    apply mp (mp (mp (disj_elim (¬A) B (A → B)) ?case_na) ?case_b) hor
+    case case_na =>
+      apply deduction
+      intro hna
+      apply deduction
+      intro ha
+      exact mp (ex_falso_classical B) (mp (mp (neg_elim A) hna) ha)
+    case case_b =>
+      apply deduction
+      intro hb
+      apply deduction
+      intro _
+      exact hb
+  exact mp (mp (iff_intro (A → B) (¬A ∨ B)) hfwd) hbwd
 
 theorem de_morgan_conj (A B : Formula) : is_provable (¬(A ∧ B) ↔ (¬A ∨ ¬B)) := by
-  sorry
+  have hfwd : is_provable (¬(A ∧ B) → (¬A ∨ ¬B)) := by
+    apply deduction
+    intro hnab
+    apply mp (proof_by_contradiction (¬A ∨ ¬B))
+    apply deduction
+    intro nnanb
+    have ha : is_provable A := by
+      apply mp (proof_by_contradiction A)
+      apply deduction
+      intro hna
+      exact mp (mp (neg_elim (¬A ∨ ¬B)) nnanb) (mp (disj_intro_left (¬A) (¬B)) hna)
+    have hb : is_provable B := by
+      apply mp (proof_by_contradiction B)
+      apply deduction
+      intro hnb
+      exact mp (mp (neg_elim (¬A ∨ ¬B)) nnanb) (mp (disj_intro_right (¬A) (¬B)) hnb)
+    have hab : is_provable (A ∧ B) := mp (mp (conj_intro A B) ha) hb
+    exact mp (mp (neg_elim (A ∧ B)) hnab) hab
+  have hbwd : is_provable ((¬A ∨ ¬B) → ¬(A ∧ B)) := by
+    exact disj_neg A B
+  exact mp (mp (iff_intro (¬(A ∧ B)) (¬A ∨ ¬B)) hfwd) hbwd
 
 theorem neg_imp_iff_conj_neg (A B : Formula) : is_provable (¬(A → B) ↔ (A ∧ ¬B)) := by
-  sorry
+  have hfwd : is_provable (¬(A → B) → (A ∧ ¬B)) := by
+    apply deduction
+    intro hnab
+    have ha : is_provable A := by
+      apply mp (proof_by_contradiction A)
+      apply deduction
+      intro hna
+      have hab : is_provable (A → B) := by
+        apply deduction
+        intro ha'
+        exact mp (ex_falso_classical B) (mp (mp (neg_elim A) hna) ha')
+      exact mp (mp (neg_elim (A → B)) hnab) hab
+    have hnb : is_provable (¬B) := by
+      apply mp (neg_intro B)
+      apply deduction
+      intro hb
+      have hab : is_provable (A → B) := by
+        apply deduction
+        intro _
+        exact hb
+      exact mp (mp (neg_elim (A → B)) hnab) hab
+    exact mp (mp (conj_intro A (¬B)) ha) hnb
+  have hbwd : is_provable ((A ∧ ¬B) → ¬(A → B)) := by
+    apply deduction
+    intro hanb
+    have ha : is_provable A := mp (conj_elim_left A (¬B)) hanb
+    have hnb : is_provable (¬B) := mp (conj_elim_right A (¬B)) hanb
+    apply mp (neg_intro (A → B))
+    apply deduction
+    intro hab
+    have hb : is_provable B := mp hab ha
+    exact mp (mp (neg_elim B) hnb) hb
+  exact mp (mp (iff_intro (¬(A → B)) (A ∧ ¬B)) hfwd) hbwd
 
 theorem contrapositive_equiv (A B : Formula) : is_provable ((A → B) ↔ (¬B → ¬A)) := by
-  sorry
+  have hfwd : is_provable ((A → B) → (¬B → ¬A)) := by
+    exact imp_contrapositive A B
+  have hbwd : is_provable ((¬B → ¬A) → (A → B)) := by
+    apply deduction
+    intro hna_nb
+    apply deduction
+    intro ha
+    apply mp (proof_by_contradiction B)
+    apply deduction
+    intro hnb
+    have hna : is_provable (¬A) := mp hna_nb hnb
+    exact mp (mp (neg_elim A) hna) ha
+  exact mp (mp (iff_intro (A → B) (¬B → ¬A)) hfwd) hbwd
 
 theorem dist_imp_conj (A B C : Formula) : is_provable ((A → (B ∧ C)) ↔ ((A → B) ∧ (A → C))) := by
-  sorry
+  have hfwd : is_provable ((A → (B ∧ C)) → ((A → B) ∧ (A → C))) := by
+    apply deduction
+    intro h
+    have hab : is_provable (A → B) := by
+      apply deduction
+      intro ha
+      exact mp (conj_elim_left B C) (mp h ha)
+    have hac : is_provable (A → C) := by
+      apply deduction
+      intro ha
+      exact mp (conj_elim_right B C) (mp h ha)
+    exact mp (mp (conj_intro (A → B) (A → C)) hab) hac
+  have hbwd : is_provable (((A → B) ∧ (A → C)) → (A → (B ∧ C))) := by
+    apply deduction
+    intro h
+    apply deduction
+    intro ha
+    have hb : is_provable B := mp (mp (conj_elim_left (A → B) (A → C)) h) ha
+    have hc : is_provable C := mp (mp (conj_elim_right (A → B) (A → C)) h) ha
+    exact mp (mp (conj_intro B C) hb) hc
+  exact mp (mp (iff_intro (A → (B ∧ C)) ((A → B) ∧ (A → C))) hfwd) hbwd
 
 theorem peirce_law (A B : Formula) : is_provable (((A → B) → A) → A) := by
-  sorry
+  apply deduction
+  intro h_peirce
+  apply mp (proof_by_contradiction A)
+  apply deduction
+  intro hna
+  have hab : is_provable (A → B) := by
+    apply deduction
+    intro ha
+    exact mp (ex_falso_classical B) (mp (mp (neg_elim A) hna) ha)
+  have ha : is_provable A := mp h_peirce hab
+  exact mp (mp (neg_elim A) hna) ha
 
 end Propositional.Classical
--- Propositional/Prop_Intuitionistic.lean
+++ Propositional/Prop_Intuitionistic.lean
@@ -13,27 +13,145 @@ axiom ex_falso (A : Formula) : is_provable (Formula.bot → A)
 
 -- Theorems
 theorem bot_iff_conj_neg (A : Formula) : is_provable (Formula.bot ↔ (A ∧ ¬A)) := by
-  sorry
+  have hfwd : is_provable (Formula.bot → (A ∧ ¬A)) := by
+    exact ex_falso (A ∧ ¬A)
+  have hbwd : is_provable ((A ∧ ¬A) → Formula.bot) := by
+    apply deduction
+    intro ana
+    have ha: is_provable (A) := by
+      exact mp (conj_elim_left A (¬A)) ana
+    have hna: is_provable (¬A) := by
+      exact mp (conj_elim_right A (¬A)) ana
+    exact mp (mp (neg_elim A) hna) ha
+  exact mp (mp (iff_intro (Formula.bot) ((A ∧ ¬A))) hfwd) hbwd
 
 theorem neg_to_imp (A B : Formula) : is_provable (¬A → (A → B)) := by
-  sorry
+  apply deduction
+  intro hna
+  apply deduction
+  intro ha
+  have hf: is_provable (Formula.bot) := by
+    exact mp (mp (neg_elim A) hna) ha
+  exact mp (ex_falso B) hf
 
 theorem neg_disj_to_imp (A B : Formula) : is_provable ((¬A ∨ B) → (A → B)) := by
-  sorry
+  apply deduction
+  intro h
+  apply deduction
+  intro ha
+  have h_cases : is_provable ((¬A → B) → ((B → B) → ((¬A ∨ B) → B))) := by
+    exact disj_elim (¬A) B B
+  have h_case_na : is_provable (¬A → B) := by
+    apply deduction
+    intro hna
+    exact mp (mp (neg_to_imp A B) hna) ha
+  have h_case_b : is_provable (B → B) := by
+    apply deduction
+    intro hb
+    exact hb
+  exact mp (mp (mp h_cases h_case_na) h_case_b) h
 
 theorem disj_to_neg_imp (A B : Formula) : is_provable ((A ∨ B) → (¬A → B)) := by
-  sorry
+  apply deduction
+  intro h
+  apply deduction
+  intro hna
+  have h_cases : is_provable ((A → (¬A → B)) → ((B → (¬A → B)) → ((A ∨ B) → (¬A → B)))) := by
+    exact disj_elim A B (¬A → B)
+  have h_case_a : is_provable (A → (¬A → B)) := by
+    apply deduction
+    intro ha
+    apply deduction
+    intro hna
+    have hf: is_provable (Formula.bot) := by
+      exact mp (mp (neg_elim A) hna) ha
+    exact mp (ex_falso B) hf
+  have h_case_b : is_provable (B → (¬A → B)) := by
+    apply deduction
+    intro hb
+    apply deduction
+    intro hna
+    exact hb
+  have h_imp_na_b: is_provable (¬A → B) := by
+    exact mp (mp (mp h_cases h_case_a) h_case_b) h
+  exact mp h_imp_na_b hna
 
 theorem neg_imp_to_double_neg (A B : Formula) : is_provable (¬(A → B) → ¬¬A) := by
-  sorry
+  apply deduction
+  intro h
+  apply mp (neg_intro (¬A))
+  apply deduction
+  intro hna
+  have hab: is_provable (A → B) := by
+    apply deduction
+    intro ha
+    have hf: is_provable (Formula.bot) := by
+      exact mp (mp (neg_elim A) hna) ha
+    exact mp (ex_falso B) hf
+  exact mp (mp (neg_elim (A → B)) h) hab
 
 theorem double_neg_imp_equiv (A B : Formula) : is_provable (¬¬(A → B) ↔ (¬¬A → ¬¬B)) := by
-  sorry
+  have hfwd : is_provable (¬¬(A → B) → (¬¬A → ¬¬B)) := by
+    apply deduction
+    intro hnnab
+    apply deduction
+    intro hnna
+    apply mp (neg_intro (¬B))
+    apply deduction
+    intro hnb
+    have hnb_to_hnab : is_provable (¬B → ¬(A → B)) := by
+      apply deduction
+      intro hnb'
+      apply mp (neg_intro (A → B))
+      apply deduction
+      intro hab
+      have hna : is_provable (¬A) := by
+        exact mp (mp (imp_contrapositive A B) hab) hnb'
+      exact mp (mp (neg_elim (¬A)) hnna) hna
+    have hnab : is_provable (¬(A → B)) := by
+      exact mp hnb_to_hnab hnb
+    exact mp (mp (neg_elim (¬(A → B))) hnnab) hnab
+  have hbwd : is_provable ((¬¬A → ¬¬B) → ¬¬(A → B)) := by
+    apply deduction
+    intro hnn_imp
+    apply mp (neg_intro (¬(A → B)))
+    apply deduction
+    intro hnab
+    have hnna : is_provable (¬¬A) := by
+      exact mp (neg_imp_to_double_neg A B) hnab
+    have hnnb : is_provable (¬¬B) := by
+      exact mp hnn_imp hnna
+    have hnb : is_provable (¬B) := by
+      exact mp (imp_neg_elim A B) hnab
+    exact mp (mp (neg_elim (¬B)) hnnb) hnb
+  exact mp (mp (iff_intro (¬¬(A → B)) (¬¬A → ¬¬B)) hfwd) hbwd
 
 theorem disj_bot_iff_self (A : Formula) : is_provable ((A ∨ Formula.bot) ↔ A) := by
-  sorry
+  have hfwd : is_provable ((A ∨ Formula.bot) → A) := by
+    apply deduction
+    intro h
+    have h_cases : is_provable ((A → A) → (Formula.bot → A) → (A ∨ Formula.bot) → A) := by
+      exact disj_elim A (Formula.bot) A
+    have h_id : is_provable (A → A) := by
+      apply Propositional.Minimal.self_imp
+    have h_bot : is_provable (Formula.bot → A) := by
+      exact ex_falso A
+    exact mp (mp (mp h_cases h_id) h_bot) h
+  have hbwd : is_provable (A → (A ∨ Formula.bot)) := by
+    apply deduction
+    intro ha
+    exact mp (disj_intro_left A (Formula.bot)) ha
+  exact mp (mp (iff_intro (A ∨ Formula.bot) A) hfwd) hbwd
 
 theorem conj_bot_iff_bot (A : Formula) : is_provable ((A ∧ Formula.bot) ↔ Formula.bot) := by
-  sorry
+  have hfwd : is_provable ((A ∧ Formula.bot) → Formula.bot) := by
+    apply deduction
+    intro h
+    exact mp (conj_elim_right A Formula.bot) h
+  have hbwd : is_provable (Formula.bot → (A ∧ Formula.bot)) := by
+    apply deduction
+    intro hf
+    exact mp (ex_falso (A ∧ Formula.bot)) hf
+  exact mp (mp (iff_intro (A ∧ Formula.bot) Formula.bot) hfwd) hbwd
 
 end Propositional.Intuitionistic
